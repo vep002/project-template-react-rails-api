3ec1c05654322b12a768e98efe2f9c1848abf59b
Victoria <vep002@gmail.com>
2021-07-12 10:51:53 -0500

    login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :entries
 
     validates :username, uniqueness: {case_sensitive: false}
     validates :password, length: {in: 5..20}
